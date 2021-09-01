720dd5da87f2735f150ba1bfbb49036d6797ce80
Vaggos <37916133+vaggos542@users.noreply.github.com>
2021-09-01 18:52:14 +0300

    Add files via upload

---
+++ Database/fun joins.sql
@@ -0,0 +1,66 @@
+﻿CREATE OR REPLACE FUNCTION carshop.transaction_date(varchar(45)) 
+RETURNS TABLE (date varchar,idcars int, brand varchar, model varchar, color varchar, price numeric, condition varchar, idcustomers int, firstname varchar, lastname varchar, country varchar) AS $$
+
+SELECT s.date,r.idcars,r.brand,r.model,r.color,r.price,r.condition,c.idcustomers,c.firstname,c.lastname,c.country FROM carshop.cars AS r
+JOIN carshop.sales AS s ON r.idcars = s.idcars 
+JOIN carshop.customers AS c ON s.idcustomers = c.idcustomers  WHERE s.date = $1; 
+$$ LANGUAGE SQL
+
+
+SELECT * FROM carshop.transaction_date('8/11/2020') 
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION carshop.transaction_color(varchar(45))
+RETURNS TABLE (date varchar,idcars int, brand varchar, model varchar, color varchar, price numeric, condition varchar, idcustomers int, firstname varchar, lastname varchar, country varchar) AS $$
+SELECT s.date,r.idcars,r.brand,r.model,r.color,r.price,r.condition,c.idcustomers,c.firstname,c.lastname,c.country FROM carshop.cars AS r
+JOIN carshop.sales AS s ON r.idcars = s.idcars 
+JOIN carshop.customers AS c ON s.idcustomers = c.idcustomers  WHERE r.color = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.transaction_color('Red') 
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION carshop.transaction_phone(varchar(45))
+RETURNS TABLE (date varchar,idcars int, brand varchar, model varchar, color varchar, price numeric, condition varchar, idcustomers int, firstname varchar, lastname varchar, country varchar) AS $$
+SELECT s.date,r.idcars,r.brand,r.model,r.color,r.price,r.condition,c.idcustomers,c.firstname,c.lastname,c.country FROM carshop.cars AS r
+JOIN carshop.sales AS s ON r.idcars = s.idcars 
+JOIN carshop.customers AS c ON s.idcustomers = c.idcustomers  WHERE c.phone = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.transaction_phone('6940400404')
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+CREATE OR REPLACE FUNCTION carshop.transaction_country(varchar(45))
+RETURNS TABLE (date varchar,idcars int, brand varchar, model varchar, color varchar, price numeric, condition varchar, idcustomers int, firstname varchar, lastname varchar, country varchar) AS $$
+SELECT s.date,r.idcars,r.brand,r.model,r.color,r.price,r.condition,c.idcustomers,c.firstname,c.lastname,c.country FROM carshop.cars AS r
+JOIN carshop.sales AS s ON r.idcars = s.idcars 
+JOIN carshop.customers AS c ON s.idcustomers = c.idcustomers  WHERE c.country = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.transaction_country('ES')
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION carshop.transaction_brand(varchar(45))
+RETURNS TABLE (date varchar,idcars int, brand varchar, model varchar, color varchar, price numeric, condition varchar, idcustomers int, firstname varchar, lastname varchar, country varchar) AS $$
+SELECT s.date,r.idcars,r.brand,r.model,r.color,r.price,r.condition,c.idcustomers,c.firstname,c.lastname,c.country FROM carshop.cars AS r
+JOIN carshop.sales AS s ON r.idcars = s.idcars 
+JOIN carshop.customers AS c ON s.idcustomers = c.idcustomers  WHERE r.brand = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.transaction_brand('Tesla')
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION carshop.transaction_gender(varchar(45))
+RETURNS TABLE (date varchar,idcars int, brand varchar, model varchar, color varchar, price numeric, condition varchar, idcustomers int, firstname varchar, lastname varchar, country varchar, gender varchar) AS $$
+SELECT s.date,r.idcars,r.brand,r.model,r.color,r.price,r.condition,c.idcustomers,c.firstname,c.lastname,c.country,c.gender FROM carshop.cars AS r
+JOIN carshop.sales AS s ON r.idcars = s.idcars 
+JOIN carshop.customers AS c ON s.idcustomers = c.idcustomers  WHERE c.gender = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.transaction_gender('Male')
---
+++ Database/stored proc.sql
@@ -0,0 +1,90 @@
+﻿------------------------------------------------------------------------------------------------
+                                    carshop.cars
+------------------------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION carshop.car_brand(varchar(45)) 
+RETURNS SETOF carshop.cars AS $$
+SELECT * FROM carshop.cars WHERE brand = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.car_brand('BMW');
+
+
+CREATE OR REPLACE FUNCTION carshop.car_model(varchar(45)) 
+RETURNS SETOF carshop.cars AS $$
+SELECT * FROM carshop.cars WHERE model = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.car_model('S');
+
+CREATE OR REPLACE FUNCTION carshop.car_color(varchar(45)) 
+RETURNS SETOF carshop.cars AS $$
+SELECT * FROM carshop.cars WHERE color = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.car_color('Silver');
+SELECT * FROM carshop.car_color('Silver') INTERSECT SELECT * FROM carshop.car_brand('Ford');
+
+
+CREATE OR REPLACE FUNCTION carshop.car_price(numeric) 
+RETURNS SETOF carshop.cars AS $$
+SELECT * FROM carshop.cars WHERE price = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.car_price(13.500);
+
+
+CREATE OR REPLACE FUNCTION carshop.car_condition(varchar(45)) 
+RETURNS SETOF carshop.cars AS $$
+SELECT * FROM carshop.cars WHERE condition = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.car_condition('Used');
+
+
+
+------------------------------------------------------------------------------------------------
+                                    carshop.customers
+------------------------------------------------------------------------------------------------
+
+
+CREATE OR REPLACE FUNCTION carshop.cus_firstname(varchar(45)) 
+RETURNS SETOF carshop.customers AS $$
+SELECT * FROM carshop.customers WHERE firstname = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.cus_firstname('Nick');
+
+
+CREATE OR REPLACE FUNCTION carshop.cus_lastname(varchar(45)) 
+RETURNS SETOF carshop.customers AS $$
+SELECT * FROM carshop.customers WHERE lastname = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.cus_lastname('Papadopoulos'); 
+
+CREATE OR REPLACE FUNCTION carshop.cus_country(varchar(45)) 
+RETURNS SETOF carshop.customers AS $$
+SELECT * FROM carshop.customers WHERE country = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.cus_country('GR')
+
+
+CREATE OR REPLACE FUNCTION carshop.cus_gender(varchar(45)) 
+RETURNS SETOF carshop.customers AS $$
+SELECT * FROM carshop.customers WHERE gender = $1;
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.cus_gender('Other')
+
+------------------------------------------------------------------------------------------------
+                                    carshop.sales
+------------------------------------------------------------------------------------------------
+
+
+CREATE OR REPLACE FUNCTION carshop.sales_date(varchar(45)) 
+RETURNS SETOF carshop.sales AS $$
+SELECT * FROM carshop.sales WHERE date = $1; 
+$$ LANGUAGE SQL
+
+SELECT * FROM carshop.sales_date('9/10/2020') 
